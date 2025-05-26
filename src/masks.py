8145debd6165d2e7a38c22336a1727c8543c8fef
Your Name <you@example.com>
2025-05-26 22:37:36 +0300

    Выполнены функции get_mask_card_number и get_mask_account из дз 9.1

---
+++ src/masks.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+
+def get_mask_card_number(card_number: str) -> Optional[str]:
+    """Функция, принимает на вход номер карты и возвращает ее маску."""
+
+    card_result: str
+    if len(card_number) == 16:
+        new_card_number = card_number[:6] + (len(card_number[6:-4]) * "*") + card_number[-4:]
+        card_result = " ".join(new_card_number[i * 4 : (i + 1) * 4] for i in range(4))
+        return card_result
+    else:
+        #print("Номер карты введен неправильно")
+        return 'Номер карты введен неправильно'
+
+
+def get_mask_account(count_number: str) -> Optional[str]:
+    """Функция, принимает на вход номер счёта и возвращает его маску."""
+
+    count_result: str
+    if len(count_number) == 20:
+        count_result = (len(card_number[-6:-4]) * "*") + count_number[-4:]
+        return count_result
+    else:
+      #  print("Номер счета введен неправильно")
+        return 'Номер счета введен неправильно'
+
+
+card_number = input("Введите номер карты:")
+card_code = get_mask_card_number(card_number)
+print(card_code)
+
+count_number = input("Введите номер счета:")
+count_code = get_mask_account(count_number)
+print(count_code)
