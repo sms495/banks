63fdee199e8569dada5997f3ac615bba4969077f
Your Name <you@example.com>
2025-07-15 02:27:39 +0300

    Модуль 10.2

--- src/widget.py
+++ src/widget.py
@@ -7,7 +7,6 @@ def mask_account_card(user_number: str) -> Optional[str]:
 
    if 'счет' in user_number.lower():
        count_result = user_number[:5] + (len(user_number[-6:-4]) * "*") + user_number[-4:]
-       #return count_result
    else:
        card_number =  user_number[-16:]
        new_card_number = card_number[:6] + (len(card_number[6:-4]) * "*") + card_number[-4:]
@@ -20,16 +19,6 @@ def mask_account_card(user_number: str) -> Optional[str]:
 
 def get_date(user_date: str) -> Optional[str]:
     """Функция, которая работает с датой вводимой с клавиатуры и возвращает строку в формате "ДД.ММ.ГГГГ"""
-    #2024-03-11T02:26:18.671407
     new_date = user_date[8:10] + "." + user_date[5:7] + "." +user_date[:4]
 
     return new_date
-
-
-user_number = input("Введите номер счета или данные карты:")
-account_card = mask_account_card(user_number)
-print(account_card)
-
-user_date = input("Введите дату:")
-current_datetime = get_date(user_date)
-print(current_datetime)
