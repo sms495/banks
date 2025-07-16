282d9c67a9fad6afb500a35b4bcf10aec4c03278
Your Name <you@example.com>
2025-07-16 23:11:26 +0300

    Модуль 10.2 (Добавлены новые фикстуры)

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,26 @@ import pytest
 @pytest.fixture
 def test_expected_count():
     return 2
+
+
+@pytest.fixture
+def sort_date_false():
+    return [
+                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                {'id': 939719570, 'state': 'EXECUTED', 'date': '2020-06-30T02:08:58.425572'},
+                {'id': 615064591, 'state': 'CANCELED', 'date': '2021-10-14T08:21:33.419441'},
+                {'id': 594226727, 'state': 'NONE', 'date': '2022-09-12T21:27:25.241689'},
+                {'id': 615064591, 'state': 'NONE', 'date': '2023-10-14T08:21:33.419441'}]
+
+
+@pytest.fixture
+def sort_date_true():
+    return [
+                {'id': 615064591, 'state': 'NONE', 'date': '2023-10-14T08:21:33.419441'},
+                {'id': 594226727, 'state': 'NONE', 'date': '2022-09-12T21:27:25.241689'},
+                {'id': 615064591, 'state': 'CANCELED', 'date': '2021-10-14T08:21:33.419441'},
+                {'id': 939719570, 'state': 'EXECUTED', 'date': '2020-06-30T02:08:58.425572'},
+                {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            ]
