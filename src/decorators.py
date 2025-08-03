6cddec3cd59953f3488ce0bfa71f90e07a130db4
Your Name <you@example.com>
2025-08-03 22:42:34 +0300

    Модуль 11.2 - Декораторы

---
+++ src/decorators.py
@@ -0,0 +1,41 @@
+import functools
+import logging
+
+
+def log(filename=None):
+    """Декоратор для логирования функции, а также просмотра ее результатов/ошибок."""
+    logger = logging.getLogger(__name__)
+
+    if filename:
+        handler = logging.FileHandler(filename)
+        logger.addHandler(handler)
+    else:
+        handler = logging.StreamHandler()
+        logger.addHandler(handler)
+
+    logger.setLevel(logging.INFO)
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                # Логиреум начало работы функцимм
+                logger.info(f"Starting {func.__name__} with args: {args} kwargs: {kwargs}")
+                print(f"Starting {func.__name__} with args: {args} kwargs: {kwargs}")
+
+                result = func(*args, **kwargs)
+
+                # Логируем успешное окончание работы функции
+                logger.info(f"{func.__name__} ok: Result: {result}")
+                print(f"{func.__name__} ok: Result: {result}")
+                return result
+
+            except Exception as e:
+                # Логируем ошибку
+                logger.error(f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}")
+                print(f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}")
+                raise
+
+        return wrapper
+
+    return decorator
